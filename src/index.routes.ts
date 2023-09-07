fc43c77a0ed07e38ccbed12483f6238538c909ca
ramses-ayala <ramses0595@gmail.com>
2023-09-06 21:21:48 -0500

    adjustment for routes

--- src/index.routes.ts
+++ src/index.routes.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { routesSignUp } from "./routes/signUp/signUp.routes";
 import { routesSignIn } from "./routes/signIn/signIn.routes";
 import { routesTodos } from "./routes/todos/todos.routes";
-import { tokenValidator } from "./utils/tokenValidator";
+
 
 const routes = Router();
 
 routes.use(routesSignUp);
 routes.use(routesSignIn);
-routes.use('/todos', tokenValidator, routesTodos);
+routes.use('/todos', routesTodos);
 
 export {routes};
--- src/routes/todos/todos.routes.ts
+++ src/routes/todos/todos.routes.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 const routesTodos = Router();
 
+import { tokenValidator } from '../../utils/tokenValidator';
+
 import { getTodosById } from '../../controllers/todosController/getTodosById.controller';
 
-routesTodos.get("/getTodoById", getTodosById);
+routesTodos.get("/getTodoById", tokenValidator ,getTodosById);
 
 export {routesTodos};
