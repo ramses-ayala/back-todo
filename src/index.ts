01f7a8162329258e19cc7b73dde849355330077a
ramses-ayala <ramses0595@gmail.com>
2023-10-21 17:44:16 -0500

    adjustments to the endpoint sigUp for improving if email exists

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
@@ -9,6 +10,7 @@ import { routes } from "./index.routes";
 app.set("port", process.env.PORT || 8000);
 
 // middlewares
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.json());
 app.use(morgan("dev"));
 
